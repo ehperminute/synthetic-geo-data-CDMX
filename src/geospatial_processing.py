51369524b0dd756d6e75481f2fd4bf1ec085abc5
ehperminute <87844955+ehperminute@users.noreply.github.com>
2025-12-14 15:05:31 -0600

    Update geospatial_processing.py

--- src/geospatial_processing.py
+++ src/geospatial_processing.py
@@ -48,7 +48,7 @@ def aggregate_risk(panel_df, students_df, colonias_df):
 
     # 3) Compute average risk per colonia
     risk_df = merged.groupby("colonia_id")["ever_dropped"].mean().reset_index()
-    risk_map_df = risk.merge(colonias_df, on="colonia_id")
+    risk_map_df = risk_df.merge(colonias_df, on="colonia_id")
 
 
     return risk_map_df
